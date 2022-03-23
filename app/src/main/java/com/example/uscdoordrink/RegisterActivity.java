6caa238fab801f7cad1458cae31b5c09c9ddcbfc
rahilbharatram <rbharatr@usc.edu>
2022-03-23 16:04:44 -0700

    register page completed

--- app/src/main/java/com/example/uscdoordrink/RegisterActivity.java
+++ app/src/main/java/com/example/uscdoordrink/RegisterActivity.java
@@ -3,6 +3,8 @@ package com.example.uscdoordrink;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Spinner isCustomerSpinner = (Spinner) findViewById(R.id.spinnerCustomer);
+
+        ArrayAdapter<String> isCustomerAdapter = new ArrayAdapter<>(RegisterActivity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.registerPageOptions));
+        isCustomerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        isCustomerSpinner.setAdapter(isCustomerAdapter);
     }
 }
