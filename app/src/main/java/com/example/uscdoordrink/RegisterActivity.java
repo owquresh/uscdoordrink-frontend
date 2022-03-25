e574240a1ad094f2a71e065b3adff23a3463b074
rahilbharatram <rbharatr@usc.edu>
2022-03-24 20:23:33 -0700

    adding backend for Register

--- app/src/main/java/com/example/uscdoordrink/RegisterActivity.java
+++ app/src/main/java/com/example/uscdoordrink/RegisterActivity.java
@@ -11,7 +11,7 @@ import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    Button buttonRegisterBack;
+    Button buttonRegisterBack, buttonRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +19,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         buttonRegisterBack = (Button) findViewById(R.id.buttonRegisterBack);
         Spinner isCustomerSpinner = (Spinner) findViewById(R.id.spinnerCustomer);
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
 
         ArrayAdapter<String> isCustomerAdapter = new ArrayAdapter<>(RegisterActivity.this,
                 android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.registerPageOptions));
@@ -26,5 +27,11 @@ public class RegisterActivity extends AppCompatActivity {
         isCustomerSpinner.setAdapter(isCustomerAdapter);
 
         buttonRegisterBack.setOnClickListener(view -> startActivity(new Intent(RegisterActivity.this, MainActivity.class)));
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
