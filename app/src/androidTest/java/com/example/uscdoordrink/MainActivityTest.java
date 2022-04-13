a70fcf7975ae33791d1963d58d77a1a0ea23d18f
rahilbharatram <rbharatr@usc.edu>
2022-04-13 11:20:28 -0700

    added espresso test cases for mainActivity

--- app/src/androidTest/java/com/example/uscdoordrink/MainActivityTest.java
+++ app/src/androidTest/java/com/example/uscdoordrink/MainActivityTest.java
@@ -62,6 +62,7 @@ public class MainActivityTest {
         onView(ViewMatchers.withId(R.id.login)).check(matches(isDisplayed()));
     }
 
+    
     @Test
     public void clickRegisterButton_opensRegisterPage() throws Exception {
         Espresso.onView(ViewMatchers.withId(R.id.buttonRegister)).perform(ViewActions.click());
